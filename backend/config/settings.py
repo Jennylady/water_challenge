74e2f4357fc149ac50bbf1ddbcaaa50d31618d79
raph2611toky <raphaeltokinandrasana@gmail.com>
2026-07-17 22:08:48 +0300

    commit change email default from

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -181,7 +181,7 @@ EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', 'False').lower() == 'true'
 EMAIL_USE_SSL = os.getenv('EMAIL_USE_SSL', 'True').lower() == 'true'
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
-DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL', f'Water Challenge')
+DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
